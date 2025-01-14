6a2d6d352e9349b77eb91cfdfe5d49f5c99f7f4a
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-01-14 13:48:55 -0500

    i

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {2, 5, 6},
+                {7, 9, 8}
+        };
+
+        for (int col = 0; col < array[0].length; col++){
+            int colSum = 0;
+            for (int row = 0; row < array.length; row++){
+                colSum += array[row][col];
+            }
+            System.out.println(colSum);
+        }
+    }
 }
